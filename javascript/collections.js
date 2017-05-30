f9e15f9e61fefb7e4464e6a386734c27733906cb
tinydalek <estajt@gmail.com>
2017-05-30 14:19:04 +1000

    carousel fixed, image sizes fixed

--- javascript/collections.js
+++ javascript/collections.js
@@ -1,8 +1,12 @@
 // Javascript Task 10, Functionality for the user to control movement through images
 
-$(document).ready(function() {
+//$('#starwars', '#universe', '#avengers').carousel("pause");
+
+
+
+/* $(document).ready(function() {
     $('#starwars', '#universe', '#avengers').carousel( {
         pause: true,
         interval: false,
     });
-});
+}); */
